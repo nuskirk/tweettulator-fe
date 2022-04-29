7d48fc23ed3cb18457a70cce0d4457b983e67df5
nuskirk <kirk@nustechnology.com>
2022-04-29 16:09:49 +0700

    Update base URL & footer

--- src/api/base_client.ts
+++ src/api/base_client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function createClient() {
   const client = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: process.env.REACT_APP_BASE_URL,
   });
   client.interceptors.response.use(
     (res) => res,
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,20 @@ import React from "react";
 type FooterProps = React.HTMLAttributes<HTMLDivElement>;
 export default function FooterComponent({ className, ...rest }: FooterProps) {
   return (
-    <footer {...rest} className={clsx(className, "h-10")}>
-      Copyright (c) {new Date().getFullYear()}
+    <footer
+      {...rest}
+      className={clsx(className)}
+      style={{ backgroundColor: "#f1f1f1" }}
+    >
+      <div
+        className="p-4 text-center text-gray-700"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2021 Copyright:
+        <a className="text-gray-800" href="https://nustechnology.com/">
+          NUS Technology
+        </a>
+      </div>
     </footer>
   );
 }
